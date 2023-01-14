fda84c6f2b3bdb2fab694eb4c0901562855e1b2c
anji-braimoh <anjolabraimoh@outlook.com>
2023-01-14 16:05:14 +0000

    fetched the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
+
+
 function App() {
+
+  const [poke,setPoke] = useState(0)
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const result = await fetch(pokeApi)
+      result.json().then(json =>{
+        console.log(json)
+      })
+    }
+    fetchData()
+  }, [])
 
   return (
     <div data-testid="app">
